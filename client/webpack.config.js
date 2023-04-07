02641e6ebeb8d17e698e0860d5a6adfab39dac91
moviefan322 <philman202@yahoo.com>
2023-04-07 19:59:31 -0400

    ready for deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -31,6 +31,7 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+      new GenerateSW(),
       new WebpackPwaManifest({
         name: "Just Another Text Editor",
         short_name: "JAST",
