cd1867aeea1a3bd7e5fa836076dacc5a0153dfc3
moviefan322 <philman202@yahoo.com>
2023-04-07 20:42:54 -0400

    service worker registers properly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -27,10 +27,6 @@ module.exports = () => {
         title: "Just Another Text Editor",
       }),
       new MiniCssExtractPlugin(),
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
       new GenerateSW(),
       new WebpackPwaManifest({
         name: "Just Another Text Editor",
